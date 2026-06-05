cc672e60b26b090a21bd62c8d241a8f842e06b44
*mahdi* <*mahdidehnavi0086@gmail.com*>
2026-06-06 02:57:10 +0330

    start tamrin2

---
+++ tamrin2-project/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string command ;
+    int number = 15 ;
+    while (number)
+    {
+        
+        
+    }
+    
+
+
+
+    return 0;
+}
