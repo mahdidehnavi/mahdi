988b29e9b367a805d739d1c0a23347d963ab8867
*mahdi* <*mahdidehnavi0086@gmail.com*>
2026-06-07 02:36:55 +0330

    nemone baraye eijad class dar in tamrin

---
+++ include/taha_k.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+
+class taha_k
+{
+private:
+    /* data */
+public:
+    taha_k(/* args */);
+    ~taha_k();
+};
+
+taha_k::taha_k(/* args */)
+{
+}
+
+taha_k::~taha_k()
+{
+}
