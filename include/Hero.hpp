134a35ac78b4487fdac04b811a4cf2367042f475
javadykazemlo <javadykazemloo@gmail.com>
2026-06-08 00:09:01 +0330

    fear:<Hero> add get

--- include/Hero.hpp
+++ include/Hero.hpp
@@ -3,16 +3,20 @@
 
 class Hero
 {
-private:
+protected:
     std::string name ;
     std::string role;
     int hp;
     bool alive;
-protected:
+public:
     Hero(/* args */);
     virtual void ability1(); 
     virtual void ability2();
     virtual void special_ablity();
+    string get_name() const;
+    string get_role() const;
+    int get_hp() const;
+    bool get_alive() const;
     ~Hero();
 };
 
--- src/Hero.cpp
+++ src/Hero.cpp
@@ -22,6 +22,26 @@ void Hero::special_ablity()
 
 }
 
+string Hero::get_name() const
+{
+    return name;
+}
+
+string Hero::get_role() const
+{
+    return role;
+}
+    
+int Hero::get_hp() const
+{
+    return hp;
+}
+    
+bool Hero::get_alive() const
+{
+    return alive;
+}
+
 
 Hero::~Hero()
 {
