2f3924248fbac5beb233472317fce2f80bf59bc0
*mahdi* <*mahdidehnavi0086@gmail.com*>
2026-06-07 22:23:10 +0330

    style:  eijad sakhtar baraye hero

---
+++ include/Hero.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+
+class Hero
+{
+private:
+    std::string name ;
+    std::string role;
+    int hp;
+public:
+    Hero(/* args */);
+    ~Hero();
+};
+
+Hero::Hero(/* args */)
+{
+}
+
+Hero::~Hero()
+{
+}
