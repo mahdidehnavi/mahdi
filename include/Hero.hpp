d8c3ad925ce924e79ebef436ec276c2a6bf75317
javadykazemlo <javadykazemloo@gmail.com>
2026-06-07 23:14:18 +0330

    feat:<Hero> add ability

--- include/Hero.hpp
+++ include/Hero.hpp
@@ -7,15 +7,12 @@ private:
     std::string name ;
     std::string role;
     int hp;
+    bool elive;
 public:
     Hero(/* args */);
+    virtual_void ability1();
+    virtual void ability2();
+    virtual void special_ablity();
     ~Hero();
 };
 
-Hero::Hero(/* args */)
-{
-}
-
-Hero::~Hero()
-{
-}
--- src/Hero.cpp
+++ src/Hero.cpp
@@ -2,3 +2,27 @@
 #include <string>
 
 using namespace std;
+
+Hero::Hero(/* args */)
+{
+    
+}
+
+void Hero::ability1()
+{
+
+}
+void Hero::ability2()
+{
+
+}
+void Hero::special_ablity()
+{
+
+}
+
+
+Hero::~Hero()
+{
+}
+
